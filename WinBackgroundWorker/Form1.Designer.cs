c05f41805bd9e81eae494bdc2807f73a34313324
root <gleb13386@outlook.com>
2021-12-04 12:32:09 +0300

    Добавлен и настроен элемент BackgroundWorker. Для событий: KeyPress элемента textBox1 и DoWork элемента BackgroundWorker были заданы обрабочики событий.

--- WinBackgroundWorker/Form1.Designer.cs
+++ WinBackgroundWorker/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // label1
@@ -60,6 +61,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(80, 20);
             this.textBox1.TabIndex = 2;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // progressBar1
             // 
@@ -85,6 +87,12 @@
             this.button2.TabIndex = 5;
             this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
             // 
             // Form1
             // 
@@ -112,6 +120,7 @@
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- WinBackgroundWorker/Form1.cs
+++ WinBackgroundWorker/Form1.cs
@@ -16,5 +16,30 @@ namespace WinBackgroundWorker
         {
             InitializeComponent();
         }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле должно содержать цифры");
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i;
+            i = int.Parse(e.Argument.ToString());
+            for (int j=1; j <= i; j++)
+            {
+                if (backgroundWorker1.CancellationPending)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+                System.Threading.Thread.Sleep(1000);
+                backgroundWorker1.ReportProgress((int)(j * 100 / i));
+            }
+        }
     }
 }
