768abfe284b2f3d3dcad896d393d51c14be44e6d
root <gleb13386@outlook.com>
2021-12-06 14:38:13 +0300

    Создана форма, выполнены шаги 2 - 4 упражнения 1.

--- WinAsynchDelegate/Form1.cs
+++ WinAsynchDelegate/Form1.cs
@@ -16,5 +16,14 @@ namespace WinAsynchDelegate
         {
             InitializeComponent();
         }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+                MessageBox.Show("Поле должно содержать цифры");
+            }
+        }
     }
 }
