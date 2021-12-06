33a13a2da1c4906c033e9b591b2bc089eb3b8682
root <gleb13386@outlook.com>
2021-12-06 15:03:08 +0300

    Задан метод TimeConsumingMethod и задан делегат к нему. Задан метод SetProgressDelegate.

--- WinAsynchDelegate/Form1.cs
+++ WinAsynchDelegate/Form1.cs
@@ -25,5 +25,31 @@ namespace WinAsynchDelegate
                 MessageBox.Show("Поле должно содержать цифры");
             }
         }
+        bool Cancel;
+        private void TimeConsumingMethod(int seconds)
+        {
+            for (int j = 1; j <= seconds; j++)
+            {
+                SetProgress((int)(j * 100) / seconds);
+                System.Threading.Thread.Sleep(1000);
+                if (Cancel)
+                    break;
+            }
+        }
+
+        private delegate void TimeConsumingMethodDelegate(int seconds);
+        public delegate void SetProgressDelegate(int val);
+        public void SetProgress(int val)
+        {
+            if (progressBar1.InvokeRequired)
+            {
+                SetProgressDelegate del = new SetProgressDelegate(SetProgress);
+                this.Invoke(del, new object[] { val });
+            }
+            else
+            {
+                progressBar1.Value = val;
+            }
+        }
     }
 }
