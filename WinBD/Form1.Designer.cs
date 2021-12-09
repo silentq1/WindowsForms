0e8a8cc3b721824d6064393f5b791011792025e3
root <gleb13386@outlook.com>
2021-12-09 14:25:06 +0300

    Добавлены элементы oleDbConnection и OleDbCommand.

---
+++ WinBD/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinBD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.oleDbConnection1 = new System.Data.OleDb.OleDbConnection();
+            this.oleDbCommand1 = new System.Data.OleDb.OleDbCommand();
+            this.SuspendLayout();
+            // 
+            // oleDbConnection1
+            // 
+            this.oleDbConnection1.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"C:\\Users\\gleb\\Desktop\\Практика по " +
+    "ПМ.01\\WindowsForms\\RBProduct.mdb\"";
+            // 
+            // oleDbCommand1
+            // 
+            this.oleDbCommand1.Connection = this.oleDbConnection1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Data.OleDb.OleDbConnection oleDbConnection1;
+        private System.Data.OleDb.OleDbCommand oleDbCommand1;
+    }
+}
+
