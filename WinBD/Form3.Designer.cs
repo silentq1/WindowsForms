3af3214d7d83d02655b36be2c546b3ab2666d5f5
root <gleb13386@outlook.com>
2021-12-10 09:19:21 +0300

    Удалена одна кнопка, добавлены обработчики событий click для кнопок. Добавлен BindingSource для таблицы Поставщики.

--- WinBD/Form3.Designer.cs
+++ WinBD/Form3.Designer.cs
@@ -30,10 +30,8 @@
         {
             this.rbProductDataSet1 = new WinBD.RBProductDataSet();
             this.поставщикиTableAdapter1 = new WinBD.RBProductDataSetTableAdapters.ПоставщикиTableAdapter();
-            this.FrametextBox = new System.Windows.Forms.TextBox();
             this.NametextBox = new System.Windows.Forms.TextBox();
             this.SectiontextBox = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.Nextbutton = new System.Windows.Forms.Button();
@@ -50,87 +48,72 @@
             // 
             this.поставщикиTableAdapter1.ClearBeforeFill = true;
             // 
-            // FrametextBox
-            // 
-            this.FrametextBox.Location = new System.Drawing.Point(94, 22);
-            this.FrametextBox.Name = "FrametextBox";
-            this.FrametextBox.Size = new System.Drawing.Size(100, 20);
-            this.FrametextBox.TabIndex = 0;
-            // 
             // NametextBox
             // 
-            this.NametextBox.Location = new System.Drawing.Point(94, 84);
+            this.NametextBox.Location = new System.Drawing.Point(94, 48);
             this.NametextBox.Name = "NametextBox";
             this.NametextBox.Size = new System.Drawing.Size(100, 20);
             this.NametextBox.TabIndex = 2;
             // 
             // SectiontextBox
             // 
-            this.SectiontextBox.Location = new System.Drawing.Point(94, 144);
+            this.SectiontextBox.Location = new System.Drawing.Point(94, 102);
             this.SectiontextBox.Name = "SectiontextBox";
             this.SectiontextBox.Size = new System.Drawing.Size(100, 20);
             this.SectiontextBox.TabIndex = 4;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(38, 25);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(56, 13);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Фамилия";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(59, 87);
+            this.label2.Location = new System.Drawing.Point(31, 51);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 6;
-            this.label2.Text = "Имя";
+            this.label2.Text = "Название";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(50, 147);
+            this.label3.Location = new System.Drawing.Point(50, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(38, 13);
             this.label3.TabIndex = 7;
-            this.label3.Text = "Отдел";
+            this.label3.Text = "Адрес";
             // 
             // Nextbutton
             // 
-            this.Nextbutton.Location = new System.Drawing.Point(41, 200);
+            this.Nextbutton.Location = new System.Drawing.Point(41, 177);
             this.Nextbutton.Name = "Nextbutton";
             this.Nextbutton.Size = new System.Drawing.Size(75, 23);
             this.Nextbutton.TabIndex = 8;
             this.Nextbutton.Text = "Next";
             this.Nextbutton.UseVisualStyleBackColor = true;
+            this.Nextbutton.Click += new System.EventHandler(this.Nextbutton_Click);
             // 
             // Previousbutton
             // 
-            this.Previousbutton.Location = new System.Drawing.Point(161, 200);
+            this.Previousbutton.Location = new System.Drawing.Point(159, 177);
             this.Previousbutton.Name = "Previousbutton";
             this.Previousbutton.Size = new System.Drawing.Size(75, 23);
             this.Previousbutton.TabIndex = 9;
             this.Previousbutton.Text = "Previous";
             this.Previousbutton.UseVisualStyleBackColor = true;
+            this.Previousbutton.Click += new System.EventHandler(this.Previousbutton_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 285);
+            this.ClientSize = new System.Drawing.Size(290, 230);
             this.Controls.Add(this.Previousbutton);
             this.Controls.Add(this.Nextbutton);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.SectiontextBox);
             this.Controls.Add(this.NametextBox);
-            this.Controls.Add(this.FrametextBox);
             this.Name = "Form3";
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
             ((System.ComponentModel.ISupportInitialize)(this.rbProductDataSet1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -141,10 +124,8 @@
 
         private RBProductDataSet rbProductDataSet1;
         private RBProductDataSetTableAdapters.ПоставщикиTableAdapter поставщикиTableAdapter1;
-        private System.Windows.Forms.TextBox FrametextBox;
         private System.Windows.Forms.TextBox NametextBox;
         private System.Windows.Forms.TextBox SectiontextBox;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button Nextbutton;
--- WinBD/Form3.cs
+++ WinBD/Form3.cs
@@ -16,5 +16,24 @@ namespace WinBD
         {
             InitializeComponent();
         }
+        private BindingSource postBindingSource;
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            поставщикиTableAdapter1.Fill(rbProductDataSet1.Поставщики);
+            postBindingSource = new BindingSource(rbProductDataSet1, "Поставщики");
+            NametextBox.DataBindings.Add("Text", postBindingSource, "Поставщик");
+            SectiontextBox.DataBindings.Add("Text", postBindingSource, "Адрес поставщика");
+            
+        }
+
+        private void Nextbutton_Click(object sender, EventArgs e)
+        {
+            postBindingSource.MoveNext();
+        }
+
+        private void Previousbutton_Click(object sender, EventArgs e)
+        {
+            postBindingSource.MovePrevious();
+        }
     }
 }
